80fbe35b742579ed8a96049cdebb6759f12dcca9
tiana <tianasjohnson98@gmail.com>
2022-01-03 08:04:51 -0500

    db query

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const mysql = require('mysql2');
-const express = require('express');
+const mysql = require("mysql2");
+const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -7,32 +7,27 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  });
-
 // Connect to database
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'birdgymmy',
-      database: 'election',
-    },
-    console.log('Connected to the election database.')
-  );
+  {
+    host: "localhost",
+    // Your MySQL username,
+    user: "tiajohn",
+    // Your MySQL password
+    password: "birdgymmy",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
 
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-  });
-
+  res.status(404).end();
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-  
+  console.log(`Server running on port ${PORT}`);
+});
